b7b615104dd4ad037469511657ba1a374706a84a
laurie <laurie.ghione@hotmail.fr>
2016-11-07 23:54:36 +0100

    Rejected commit: Default

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * User: Laurie Ghione
+ * Date: 07/11/16
+ * Time: 12:32
+ */
+
+require_once('FPDF/fpdf.php');
+require_once('FPDI/fpdi.php');
+require_once('function.php');
+
+date_default_timezone_set('Europe/Paris');
+setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
+$today = new DateTime();
+
+/* calculation nb days in society */
+$dateEntrance = stringToDate('2015-03-01');
+$interval = dateDiff($today,$dateEntrance);
+$nbDaysInSociety =  (12 * $interval->format('%y') + $interval->format('%m'));
+
+/* calculation my age */
+$dateOfBirth = stringToDate('1991-08-05');
+$interval = dateDiff($today,$dateOfBirth);
+$age = $interval->format('%y');
+
+/* generate pdf with new data */
+$pdf = new FPDI();
+
+/* add my pdf file*/
+$pageCount = $pdf->setSourceFile('CVLaurieGhione.pdf');
+$tplIdx = $pdf->importPage(1);
+
+$pdf->addPage();
+$pdf->useTemplate($tplIdx, 0, 0, 210);
+
+$pdf->AddFont('MyriadPro-Regular','','MyriadPro-Regular.php');
+$pdf->SetFont('MyriadPro-Regular','',9);
+
+/* display num days in society */
+$pdf->SetTextColor(0, 0, 0);
+$pdf->SetXY(151.4, 74.2);
+$pdf->Write(0, $nbDaysInSociety);
+
+/* display my age */
+$pdf->SetTextColor(255, 255, 255);
+$pdf->SetXY(187, 37.5);
+$pdf->Write(0, $age);
+
+/* Pdf download */
+$pdf->Output('D','CVLaurieGhione.pdf');
+?>
